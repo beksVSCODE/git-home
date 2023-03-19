8fea5d4e213e41a3949a6b7446ffd128786e2380
unknown <sekerbekovbeksultan2@gmail.com>
2023-03-19 23:23:11 +0600

    homework

--- variables/index.js
+++ variables/index.js
@@ -1,1 +1,45 @@
 //!1-задание
+// let names = "Джон"
+// let admin = names
+// console.log(admin);
+
+
+//!2-задание
+// let str = 'Привет, Мир!'
+// console.log(str);
+
+// let str1 = 'Привет,'
+// let str2 = 'Мир!'
+// console.log(str1 + str2);
+
+// let name = "Beka"
+// console.log("Привет, " + name);
+
+// let age = 17
+// console.log("Мне " + age + " лет");
+
+//!3-задание
+// let num = 3
+// console.log(num);
+
+// let a = 10
+// let b = 2
+// console.log(a+b,a-b,a*b,a/b);
+
+// let c = 15
+// let d = 2
+// let result = c + d
+// console.log(result);
+
+// let a = 10
+// let b = 2
+// let c = 5
+// console.log(a + b + c);
+
+
+// let a = 17
+// let b = 10
+// let c = a - b
+// let d = 7
+// let result = c + d
+// console.log(result);
