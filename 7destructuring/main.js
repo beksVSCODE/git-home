7cab1cab40667c9c3400ead192f943bd43b792a2
unknown <sekerbekovbeksultan2@gmail.com>
2023-04-25 23:13:16 +0600

    tomorrow i have to over

---
+++ 7destructuring/main.js
@@ -0,0 +1,46 @@
+//! Задание 1
+
+// let person = {
+//     name:'Beka',
+//     age:17,
+//     city:'California'
+// }
+// const { name:personName,city:personCity,age:personAge} = person
+
+// console.log(personName); 
+// console.log(personAge);
+// console.log(personCity);
+//! Задание 2
+
+// let arr = [
+//     {name:'Aidar',age:20},
+//     {name:'Dima',age:18},
+//     {name:'Uluk',age:21},
+//     {name:'Somebody',age:21},
+// ]
+
+// const [ {name:firstName,age:firstAge}] = arr
+// console.log(firstName,firstAge);
+//сразу деструктуризировал элемент
+
+//! Задание 3
+
+// let obj = {
+//     numbers:[1,2,3,4,5]
+// }
+
+// const { numbers:[firstNum,secondNum]} = obj
+// console.log(firstNum,secondNum);
+
+//! Задание 4
+
+// let person = {
+//     name:'Beka',
+//     age:17
+// }
+// const { name:newName } = person
+
+// let newPerson = {
+//     newName
+// }
+// console.log(newPerson);
